f4990ff66e28bb656e4e770d7281dd5220357a2d
mujeebishaq <[mujeebishaqg@gmail.com]>
2017-12-01 21:02:00 +0500

    initial upload to github

---
+++ DataInsertion.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InfoStore
+{
+    /// <summary>
+    /// Interaction logic for DataInsertion.xaml
+    /// </summary>
+    public partial class DataInsertion : Window
+    {
+        private object dataGrid;
+
+        public DataInsertion()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                string connectionString = ConfigurationManager.ConnectionStrings["InfoStore.Properties.Settings.StorageConnectionString"].ConnectionString;
+                string query = "INSERT INTO store VALUES (@Name, @Email, @PhoneNumber, @Location)";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    command.Parameters.AddWithValue("@Name", name.Text);
+                    command.Parameters.AddWithValue("@Email", email.Text);
+                    command.Parameters.AddWithValue("@PhoneNumber", phoneNumber.Text);
+                    command.Parameters.AddWithValue("@Location", location.Text);
+
+                    command.ExecuteNonQuery();
+                }
+                /*
+                CloseMainWindowNow();
+                var win = new MainWindow();
+                win.Show();
+                */
+                Close();
+            
+         
+       }/*
+        public void CloseMainWindowNow()
+        {
+         
+                var mainWindow = (Application.Current.MainWindow as MainWindow);
+                if (mainWindow != null)
+                {
+                    mainWindow.Close();
+                }
+         
+            
+        }*/
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InfoStore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// Project to be completed using Linq
+    /// Database must have some several fields like
+    /// Id, Name, Email, Phone Number, Location
+    /// It just stores the data/information. Never deletes anything, name depicts informationStorage eh ?
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenConnectionAndShow();
+        }
+
+        public void OpenConnectionAndShow() {
+
+            string connectionString = ConfigurationManager.ConnectionStrings["InfoStore.Properties.Settings.StorageConnectionString"].ConnectionString;
+            string query = "SELECT * FROM store";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter data = new SqlDataAdapter(query, connectionString))
+            {
+                connection.Open();
+                DataTable dt = new DataTable();
+                data.Fill(dt);
+                dataGrid.CanUserAddRows = false;
+                dataGrid.CanUserDeleteRows = false;
+                dataGrid.SelectedValuePath = "Name";
+                dataGrid.ItemsSource = dt.DefaultView;
+                dataGrid.Items.Refresh();
+
+            }
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataInsertion window = new DataInsertion();
+            window.Show();
+        }
+    }
+}
